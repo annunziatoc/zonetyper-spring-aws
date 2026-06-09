a64d09ac79767c0c3c04fbf81a0ef8e691ddc3b3
chris a <chris@Lydian.localdomain>
2026-06-09 15:20:56 -0400

    add cors origins for prod domains

--- backend/src/main/java/zonetyper/config/CorsConfig.java
+++ backend/src/main/java/zonetyper/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173",
+                        "api.zonetyper.com",
                         "https://www.zonetyper.com",
                         "https://zonetyper.com")
                 .allowedMethods("*")
